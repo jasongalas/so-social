69af492a5c16f6db394183b9270f5cccd36b59e0
jasongalas <jasongalas@gmail.com>
2024-05-20 20:37:19 -0700

    Final push

--- README.md
+++ README.md
@@ -4,19 +4,19 @@
 
 ## Description
 
-For this module, 
+For this module, we were tasked with building the backend of a fictional social media site which had thoughts rather than tweets. Using NoSQL rather than SQL, we created schemas and CRUD routes in a different yet similar fashion to what we had learned so far. This module was an exercise in actually reading the README properly, as I made extra work for myself that was thoroughly unnecessary.
 
 ## Installation
 
-
+For starters, run npm i to install all the necessary files with "mongoose" and "express". There is a seed file that preloads users that can be run with npm run seed, then you must npm run start to make the link live.
 
 ## Usage
 
-All of the magic happens in Insomnia
+All of the magic happens in Insomnia, where we can get all users, get a single user; create and delete users and well as add and remove friends. You can also see all thoughts, see a single thought; create, update, and delete thoughts as well as create and delete reactions. https://drive.google.com/file/d/1NDShfRSreslHzSlv4FZikx31HzcUyZlQ/view 
 
 ## Credits
 
-
+Shoutout to our instructor and TA, for showing me that I was working too hard and inventing issues that didn't need to a thing. 
 
 ## License
 
--- utils/seed.js
+++ utils/seed.js
@@ -10,11 +10,6 @@ connection.once('open', async () => {
     if (userCheck.length) {
       await connection.dropCollection('users');
     }
-
-    let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
-    if (thoughtCheck.length) {
-      await connection.dropCollection('thoughts');
-    }
   // Create array to hold the users
   const users = [
     {
@@ -37,58 +32,6 @@ connection.once('open', async () => {
 
   // Add users to the collection and await the results
   const userData = await User.insertMany(users);
-
-  // Add thoughts to the collection and await the results
-  const thoughts = [
-    {
-      
-      reactions: [{
-        ,
-      },
-      {
-        reactionBody: "Can't wait to dance the night away!",
-        username: userData[3].username
-      }],
-    },
-    {
-      thoughtText: "I think I'll start tracking my bench press once I hit 300 pounds.",
-      username: userData[2].username,
-      reactions: [{
-        reactionBody: "Woof lol subtle brag there ;P",
-        username: userData[1].username,
-      },
-      {
-        reactionBody: "Two plates ain't nothin'!",
-        username: userData[0].username
-      }],
-    },
-    {
-      thoughtText: "The wonderful thing about handlers is handlers are wonderful things.",
-      username: userData[1].username,
-      reactions: [{
-        reactionBody: "Can confirm.",
-        username: userData[0].username,
-      },
-      {
-        reactionBody: "Seconded!",
-        username: userData[3].username
-      }],
-    },
-    {
-      thoughtText: "My cat demands the belly rubs and then attacks me when I give them to her??",
-      username: userData[3].username,
-      reactions: [{
-        reactionBody: "Haha it's a trap!",
-        username: userData[2].username,
-      },
-      {
-        reactionBody: "Mine's the same way smh",
-        username: userData[0].username
-      }],
-    },
-  ];
-
-const thoughtData = await Thought.insertMany(thoughts);
 
   // Log out the seed data to indicate what should appear in the database
   console.info('Seeding complete! 🌱');
